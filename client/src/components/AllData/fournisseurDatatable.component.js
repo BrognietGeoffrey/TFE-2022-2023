d37d32a767460e5b2daf242f3557219f819fec5e
BrognietGeoffrey <geoffrey.brogniet@gmail.com>
2023-05-17 17:13:32 +0200

    delete loop error

--- client/src/components/AllData/fournisseurDatatable.component.js
+++ client/src/components/AllData/fournisseurDatatable.component.js
@@ -1,25 +1,16 @@
 import react from 'react';
-import { useEffect, useState } from 'react';
-import {InputText} from 'primereact/inputtext';
+import { useEffect } from 'react';
+
 import {DataTable} from 'primereact/datatable';
 import {Column} from 'primereact/column';
-import {Toast } from 'primereact/toast';
-import { Button } from 'primereact/button';
-import { FilterMatchMode, FilterOperator } from 'primereact/api';
 
-import fournisseurService from '../../services/fournisseurService';
-import compteFournisseurService from '../../services/compteFournisseurService';
+import FournisseurDataService from '../../services/fournisseurService';
 
 const FournisseurDatatable = () => {
     const [fournisseur, setFournisseur] = react.useState([]);
-    const [compteFournisseur, setCompteFournisseur] = react.useState([]);
-    const toast = react.useRef(null);
-    const [globalFilterValue1, setGlobalFilterValue1] = useState('');
 
-    const [filters1, setFilters1] = useState(null)
-
-    const getFournisseurs = () => {
-        fournisseurService.getAll().then((response) => {
+    const getFournisseur = () => {
+        FournisseurDataService.getAll().then((response) => {
             setFournisseur(response.data);
             console.log(response.data, 'response.data');
         })
@@ -28,136 +19,16 @@ const FournisseurDatatable = () => {
         });
     }
 
-    const getCompteFournisseur = () => {
-        compteFournisseurService.getAll().then((response) => {
-            setCompteFournisseur(response);
-            console.log(response, 'response.data');
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-    }
-
     useEffect(() => {
-        getFournisseurs()
-        getCompteFournisseur()
-        initFilters1();
-    }, [fournisseur, compteFournisseur]);
-
-    const onRowEditComplete = (e) => {
-        console.log(e)
-        const data = {
-            name: e.newData["fournisseur.name"],
-            email_fournisseur: e.newData["fournisseur.email_fournisseur"], 
-            adresse_fournisseur: e.newData["fournisseur.adresse_fournisseur"],
-            telephone_fournisseur: e.newData["fournisseur.telephone_fournisseur"],
-            num_fournisseur: e.newData["fournisseur.num_fournisseur"],
-            description : e.newData["fournisseur.description"],
-        }
-        const dataCompteClient = {
-            numCompteFournisseur: e.newData.numCompteFournisseur,
-            num_compte_banque: e.newData.num_compte_banque,
-        }
-        // si au moins une data de data a été modifié alors on fait appel à la fonction update
-        if (e.data.name !== e.newData["fournisseur.name"] || e.data.email_fournisseur !== e.newData["fournisseur.email_fournisseur"] || e.data.adresse_fournisseur !== e.newData["fournisseur.adresse_fournisseur"] || e.data.telephone_fournisseur !== e.newData["fournisseur.telephone_fournisseur"] || e.data.num_fournisseur !== e.newData["fournisseur.num_fournisseur"] || e.data.description !== e.newData["fournisseur.description"]) {
-        fournisseurService.update(e.data.fournisseur_id, data)
-        .then((response) => {   
-            console.log(response.data);
-            toast.current.show({ severity: 'success', summary: 'Success', detail: 'Fournisseur Updated', life: 3000 });
-            getCompteFournisseur();
-        }
-        )
-        .catch((error) => {
-            console.log(error);
-        }
-        );}
-        if (e.data.numCompteFournisseur != e.newData.numCompteFournisseur || e.data.num_compte_banque != e.newData.num_compte_banque) {
-        compteFournisseurService.update(e.data.compte_fournisseur_id, dataCompteClient)
-        .then((response) => {
-            console.log(response.data);
-            toast.current.show({ severity: 'success', summary: 'Success', detail: 'Compte Client Updated', life: 3000 });
-            getCompteFournisseur();
-        }
-        )
-        .catch((error) => {
-            console.log(error);
-            toast.current.show({ severity: 'error', summary: 'Error', detail: 'Compte Client Not Updated', life: 3000 });
-        }
-        );
-    }
-    }
-
-    const clearFilter1 = () => {
-        initFilters1();
-    }
-
-    const onGlobalFilterChange1 = (e) => {
-        const value = e.target.value;
-        let _filters1 = { ...filters1 };
-        _filters1['global'].value = value;
-
-        setFilters1(_filters1);
-        setGlobalFilterValue1(value);
-    }
-
-
-    const initFilters1 = () => {
-        setFilters1({
-            'global': { value: null, matchMode: FilterMatchMode.CONTAINS },
-            'fournisseur.name': { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
-            'fournisseur.email_fournisseur': { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
-            'fournisseur.adresse_fournisseur': { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
-            'fournisseur.telephone_fournisseur': { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
-            'fournisseur.description': { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
-            'numCompteFournisseur': { operator: FilterOperator.OR, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
-            'num_compte_banque': {  operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
-
-        });
-        setGlobalFilterValue1('');
-    }
-
-    const renderHeader1 = () => {
-        return (
-            <div className="flex justify-content-between">
-                <Button type="button" icon="pi pi-filter-slash" label="Vider les filtres" className="p-button-outlined" onClick={clearFilter1} />
-                <span className="p-input-icon-left">
-                    <i className="pi pi-search" />
-                    <InputText value={globalFilterValue1} onChange={onGlobalFilterChange1} placeholder="Rechercher..." />
-                </span>
-            </div>
-        )
-    }
-
-    const header1 = renderHeader1();
-
-
-
-
-        
-    
-
-    const textEditor = (options) => {
-        console.log(options)
-        // Message dans le dialog pour informer l'utilisateur
-        return <InputText type="text" value={options.value} onChange={(e) => options.editorCallback(e.target.value)} tooltip="Attention ! Toutes modifications entrainera des changements sur tout le facturier" tooltipOptions={{ className: 'yellow-tooltip', position: 'top' }} />;
-    }
+        getFournisseur();
+    }, []);
 
     return (
         <div>
-            <Toast ref={toast} />
-            <DataTable value={compteFournisseur} editMode="row" header={header1} onRowEditComplete={onRowEditComplete} filterDisplay="menu" globalFilterFields={['fournisseur.name', 'numCompteFournisseur', 'fournisseur.adresse_fournisseur', 'fournisseur.telephone_fournisseur','num_compte_banque', 'fournisseur.email_fournisseur', 'fournisseur.description', 'createdAt']} filters={filters1}>
-                <Column field="fournisseur.name" header="Nom" sortable editor={(options) => textEditor(options)} filter></Column>
-                <Column field="fournisseur.email_fournisseur" header="Email" sortable editor={(options) => textEditor(options)} filter ></Column>
-                <Column field="fournisseur.adresse_fournisseur" header="Adresse" sortable editor={(options) => textEditor(options)} filter> </Column>
-                <Column field="fournisseur.telephone_fournisseur" header="Téléphone" sortable editor={(options) => textEditor(options)} filter></Column>
-                <Column field="fournisseur.num_fournisseur" header="Numéro de fournisseur" sortable editor={(options) => textEditor(options)} filter></Column>
-                <Column field="fournisseur.description" header="Description" sortable editor={(options) => textEditor(options)} filter></Column>
-                <Column field="numCompteFournisseur" header="Numéro de compte" sortable editor={(options) => textEditor(options)} filter></Column>
-                <Column field="num_compte_banque" header="N° de compte en banque" sortable editor={(options) => textEditor(options)} filter></Column>
-                <Column field="createdAt" header="Date de création" body={(rowData) => { return (<div>{new Date(rowData.createdAt).toLocaleDateString()}</div>)}}></Column>
-
-
-                <Column rowEditor></Column>
+            <DataTable value={fournisseur}>
+                <Column field="name" header="Nom" sortable></Column>
+                <Column field="email_fournisseur" header="Prénom" sortable></Column>
+                
             </DataTable>
         </div>
     );
