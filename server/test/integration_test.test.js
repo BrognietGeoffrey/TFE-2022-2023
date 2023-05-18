ab52c1d5949b91757b20268ee4dd47e0b37f8e73
BrognietGeoffrey <geoffrey.brogniet@gmail.com>
2023-05-18 09:26:42 +0200

    file test

---
+++ server/test/integration_test.test.js
@@ -0,0 +1,402 @@
+const assert = require("assert");
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../index');
+let should = chai.should();
+var request = require("supertest");
+const secretKey = process.env.JWT_SECRET_KEY;
+chai.use(chaiHttp);
+const expect = chai.expect;
+const jwt = require('jsonwebtoken');
+
+
+
+const payload = {
+    role : "admin", 
+    user_id : {
+        id : 1,
+    }
+    };
+  
+  const options = {
+    expiresIn: '7d' // Définit l'expiration du JWT à une durée plus longue (7 jours dans cet exemple)
+  };
+  
+  const token = jwt.sign(payload, secretKey, options);
+
+request = request("http://localhost:4000");
+
+describe('Facturiers routes', function() {
+  describe('GET /api/facturiers', function() {
+    it('should fetch all facturiers successfully', function(done) {
+      request.get('/api/facturiers')
+        .set('Authorization', `Bearer ${token}`)
+        .end(function(err, res) {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an('array');
+          done();
+        });
+    });
+  });
+  describe('GET /api/facturiers/:id', function() {
+    it('should fetch one facturier successfully', function(done) {
+    request.get('/api/facturiers/1')
+        .set('Authorization', `Bearer ${token}`)
+        .end(function(err, res) {
+            expect(res).to.have.status(200);
+
+        res.body.facture_id.should.be.equal(15);
+        done();
+        });
+    });
+});
+});
+
+
+
+describe('Decomptes routes', function() {
+    describe('GET /api/decomptes', function() {
+      it('should fetch all decomptes successfully', function(done) {
+        request.get('/api/decomptes')
+          .set('Authorization', `Bearer ${token}`)
+          .end(function(err, res) {
+            expect(err).to.be.null;
+            expect(res).to.have.status(200);
+            expect(res.body).to.be.an('array');
+            done();
+          });
+      });
+    });
+  });
+
+  describe('Decompte routes', function() {
+    describe('GET /api/decomptes/:id', function() {
+        it('should fetch one decomptes successfully', function(done) {
+        request.get('/api/decomptes/1')
+            .set('Authorization', `Bearer ${token}`)
+            .end(function(err, res) {
+                expect(res).to.have.status(200);
+
+            res.body.type.should.be.equal("Factures")
+            done();
+            });
+        });
+    });
+    }
+);
+
+describe('objet routes', function() {
+    describe('GET /api/objet', function() {
+      it('should fetch all objet successfully', function(done) {
+        request.get('/api/objet')
+          .set('Authorization', `Bearer ${token}`)
+          .end(function(err, res) {
+            expect(err).to.be.null;
+            expect(res).to.have.status(200);
+            expect(res.body).to.be.an('array');
+            done();
+          });
+      });
+    });
+  });
+
+  describe('------- Objets routes -----------', function() {
+    describe('GET /api/objet/:id', function() {
+        it('should fetch one objet successfully', function(done) {
+        request.get('/api/objet/1')
+            .set('Authorization', `Bearer ${token}`)
+            .end(function(err, res) {
+                expect(res).to.have.status(200);
+
+            res.body.title.should.be.equal("Factures")
+            done();
+            });
+        });
+    });
+    }
+);
+
+describe('------- Factures routes -----------', function() {
+    describe('GET /api/facture', function() {
+      it('should fetch all factures successfully', function(done) {
+        request.get('/api/facture')
+          .set('Authorization', `Bearer ${token}`)
+          .end(function(err, res) {
+            expect(err).to.be.null;
+            expect(res).to.have.status(200);
+            expect(res.body).to.be.an('array');
+            done();
+          });
+      });
+    });
+  });
+  describe('------- Facture routes -----------', function() {
+    describe('GET /api/facture/:id', function() {
+        it('should fetch one objet successfully', function(done) {
+        request.get('/api/facture/4')
+            .set('Authorization', `Bearer ${token}`)
+            .end(function(err, res) {
+                expect(res).to.have.status(200);
+
+            res.body.montant.should.be.equal(2385)
+            res.body.libelle_id.should.be.equal(3)
+            done();
+            });
+        });
+    });
+    }
+);
+
+describe('------- Libelles routes -----------', function() {
+    describe('GET /api/libelle', function() {
+      it('should fetch all libelle successfully', function(done) {
+        request.get('/api/libelle')
+          .set('Authorization', `Bearer ${token}`)
+          .end(function(err, res) {
+            expect(err).to.be.null;
+            expect(res).to.have.status(200);
+            expect(res.body).to.be.an('array');
+            done();
+          });
+      });
+    });
+  });
+
+
+  describe('------- Libelle routes -----------', function() {
+    describe('GET /api/libelle/:id', function() {
+        it('should fetch one libelle successfully', function(done) {
+        request.get('/api/libelle/1')
+            .set('Authorization', `Bearer ${token}`)
+            .end(function(err, res) {
+                expect(res).to.have.status(200);
+
+            res.body.title.should.be.equal("Factures")
+           
+            done();
+            });
+        });
+    });
+    }
+);
+
+describe('------- TVA routes -----------', function() {
+    describe('GET /api/tva', function() {
+      it('should fetch all tva successfully', function(done) {
+        request.get('/api/tva')
+          .set('Authorization', `Bearer ${token}`)
+          .end(function(err, res) {
+            expect(err).to.be.null;
+            expect(res).to.have.status(200);
+            expect(res.body).to.be.an('array');
+            done();
+          });
+      });
+    });
+  }
+);
+
+describe('------- TVA routes -----------', function() {
+    describe('GET /api/tva/:id', function() {
+        it('should fetch one tva successfully', function(done) {
+        request.get('/api/tva/1')
+            .set('Authorization', `Bearer ${token}`)
+            .end(function(err, res) {
+                expect(res).to.have.status(200);
+
+            res.body.tva_value.should.be.equal("6")
+           
+            done();
+            });
+        });
+    });
+    }
+);
+
+describe('------- Comments routes -----------', function() {
+    describe('GET /api/comments', function() {
+      it('should fetch all comments successfully', function(done) {
+        request.get('/api/comments')
+          .set('Authorization', `Bearer ${token}`)
+          .end(function(err, res) {
+            expect(err).to.be.null;
+            expect(res).to.have.status(200);
+            expect(res.body).to.be.an('array');
+            done();
+          });
+      });
+    });
+  }
+);
+
+describe('------- Comments routes -----------', function() {
+    describe('POST /api/comments', function() {
+        it('should create a comment successfully', function(done) {
+            request.post('/api/comments')
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                comments: 'test',
+                userId: 1,
+                facturier_id: 1, 
+                title : 'test',
+            })
+            .end(function(err, res) {
+                expect(err).to.be.null;
+                expect(res).to.have.status(200);
+                expect(res.body).to.be.an('object');
+                done();
+            });
+        });
+        }
+    );
+});
+
+describe('------- extraits routes -----------', function() {
+    describe('GET /api/extraits', function() {
+      it('should fetch all extraits successfully', function(done) {
+        request.get('/api/extraits')
+          .set('Authorization', `Bearer ${token}`)
+          .end(function(err, res) {
+            expect(err).to.be.null;
+            expect(res).to.have.status(200);
+            expect(res.body).to.be.an('array');
+            done();
+          });
+      });
+    });
+  }
+
+);
+
+describe('------- extraits routes -----------', function() {
+    describe('GET /api/extraits/:id', function() {
+        it('should fetch one extrait successfully', function(done) {
+        request.get('/api/extraits/1')
+            .set('Authorization', `Bearer ${token}`)
+            .end(function(err, res) {
+                expect(res).to.have.status(200);
+
+            res.body.num_extrait.should.be.equal(1)
+            res.body.montant.should.be.equal(1)
+           
+            done();
+            });
+        });
+    });
+    }
+);
+
+describe('------- logs routes -----------', function() {
+    describe('GET /api/logs', function() {
+      it('should fetch all logs successfully', function(done) {
+        request.get('/api/logs')
+          .set('Authorization', `Bearer ${token}`)
+          .end(function(err, res) {
+            expect(err).to.be.null;
+            expect(res).to.have.status(200);
+            expect(res.body).to.be.an('array');
+            done();
+          });
+      });
+    });
+  }
+
+);
+
+describe('------- logs routes -----------', function() {
+    describe('POST /api/logs', function() {
+        it('should create a log successfully', function(done) {
+            request.post('/api/logs')
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                user_id: 1,
+                facturier_id: 1, 
+                description : 'Ajout d"un facturier',
+            })
+            .end(function(err, res) {
+                expect(err).to.be.null;
+                expect(res).to.have.status(200);
+                expect(res.body).to.be.an('object');
+                done();
+            });
+        });
+        }
+    );
+});
+
+describe('------- fournisseur routes -----------', function() {
+    describe('GET /api/fournisseurs', function() {
+      it('should fetch all fournisseurs successfully', function(done) {
+        request.get('/api/fournisseurs')
+          .set('Authorization', `Bearer ${token}`)
+          .end(function(err, res) {
+            expect(err).to.be.null;
+            expect(res).to.have.status(200);
+            expect(res.body).to.be.an('array');
+            done();
+          });
+      });
+    });
+  }
+)
+
+describe('------- fournisseur routes -----------', function() {
+    describe('GET /api/fournisseurs/:id', function() {
+        it('should fetch one fournisseur successfully', function(done) {
+        request.get('/api/fournisseurs/1')
+            .set('Authorization', `Bearer ${token}`)
+            .end(function(err, res) {
+                expect(res).to.have.status(200);
+
+            res.body.name.should.be.equal("Aqua1")
+            res.body.num_fournisseur.should.be.equal(123211)
+           
+            done();
+            });
+        });
+    });
+    }
+);
+
+
+describe('------- client routes -----------', function() {
+    describe('GET /api/clients', function() {
+      it('should fetch all clients successfully', function(done) {
+        request.get('/api/clients')
+          .set('Authorization', `Bearer ${token}`)
+          .end(function(err, res) {
+            expect(err).to.be.null;
+            expect(res).to.have.status(200);
+            expect(res.body).to.be.an('array');
+            done();
+          });
+      });
+    });
+  }
+)
+
+describe('------- clients routes -----------', function() {
+    describe('GET /api/clients/:id', function() {
+        it('should fetch one clients successfully', function(done) {
+        request.get('/api/clients/1')
+            .set('Authorization', `Bearer ${token}`)
+            .end(function(err, res) {
+                expect(res).to.have.status(200);
+
+            res.body.name.should.be.equal("Geoffrey")
+            res.body.firstname.should.be.equal("Brogniet")
+           
+            done();
+            });
+        });
+    });
+    }
+);
+
+
+
+
+
+
+
---
+++ server/test/unit_test.test.js
@@ -0,0 +1,446 @@
+
+// *************************************************************************************************
+// ***************************************** TEST CREATE FACTURE ***********************************
+// *************************************************************************************************
+const {validFormBodyFacture} = require('../controllers/facture.controller');
+const {validFormBodyLibelle} = require('../controllers/infosFournisseur.controller');
+const {validFormBodyObjet} = require('../controllers/infosFournisseur.controller');
+const {validFormBodyTva} = require('../controllers/tva.controller');
+var httpMocks = require('node-mocks-http');
+var assert = require('assert');
+
+describe('facture.controller', () => {
+    it('validFormBodyFacture should return true with valid body', () => {
+      let request = httpMocks.createRequest({
+        body: {
+            num_facture: "123456789",
+            num_facture_lamy: "123456789",
+            facture_date: "2020-01-01",
+            due_date: "2020-01-01",
+            montant: 100,
+            objet_id: 1,
+            libelle_id: 1,
+            tva_id: 1,
+        }
+      });
+        let response = httpMocks.createResponse();  
+        validFormBodyFacture(request.body);
+        assert.equal(response.statusCode, 200);
+
+    });
+});
+
+// Modify with valid body
+describe('facture.controller', () => {
+    it('validFormBodyFacture should return true with valid body', () => {
+        let request = httpMocks.createRequest({
+            body: {
+                facture_id : 3,
+                num_facture: "123456789",
+                num_facture_lamy: "123456789",
+                facture_date: "2020-01-01",
+                due_date: "2020-01-01",
+                montant: 100,
+                objet_id: 1,
+                libelle_id: 1,
+                tva_id: 1,
+            }
+        });
+        let response = httpMocks.createResponse();
+        validFormBodyFacture(request.body);
+        assert.equal(response.statusCode, 200);
+        
+    });
+});
+
+// Modify with invalid body
+describe('facture.controller', () => {
+    it('validFormBodyFacture should return true with valid body', () => {
+        let request = httpMocks.createRequest({
+            body: {
+                facture_id : 3,
+                
+                num_facture_lamy: "123456789",
+                facture_date: "2020-01-01",
+                due_date: "2020-01-01",
+                montant: 100,
+                objet_id: 1,
+                libelle_id: 1,
+                tva_id: 1,
+            }
+        });
+    
+        assert.throws(() => {
+            validFormBodyFacture(request.body);
+          }, Error, "Numéro de facture obligatoire"
+        );
+
+        
+    });
+});
+
+
+
+
+
+
+// Test sans aucun body 
+describe('facture.controller', () => {
+    it('validFormBodyFacture should return false with empty body', () => {
+      let request = httpMocks.createRequest({
+        undefined
+      });
+        assert.throws(() => {
+            validFormBodyFacture(request);
+          } 
+        );
+
+
+    });
+}
+);
+
+// Test sans num_facture
+describe('facture.controller', () => {
+    it('validFormBodyFacture should return false with empty num_facture', () => {
+      let request = httpMocks.createRequest({
+        body: {
+            num_facture: "",
+            num_facture_lamy: "123456789",
+            facture_date: "2020-01-01",
+            due_date: "2020-01-01",
+            montant: 100,
+            objet_id: 1,
+            libelle_id: 1,
+            tva_id: 1,
+        }
+      });
+        assert.throws(() => {
+            validFormBodyFacture(request.body);
+          }, Error, "Numéro de facture obligatoire"
+        );
+          
+    });
+}
+);
+
+// Test sans num_facture_lamy
+describe('facture.controller', () => {
+    it('validFormBodyFacture should return false with empty num_facture_lamy', () => {
+      let request = httpMocks.createRequest({
+        body: {
+            num_facture: "123456789",
+            num_facture_lamy: "",
+            facture_date: "2020-01-01",
+            due_date: "2020-01-01",
+            montant: 100,
+            objet_id: 1,
+            libelle_id: 1,
+            tva_id: 1,
+        }
+      });
+        assert.throws(() => {
+            validFormBodyFacture(request.body);
+          }, Error, "Numéro de facture Lamy obligatoire"
+        );
+          
+    });
+}
+);
+
+// Test sans facture_date
+describe('facture.controller', () => {
+    it('validFormBodyFacture should return false with empty facture_date', () => {
+      let request = httpMocks.createRequest({
+        body: {
+            num_facture: "123456789",
+            num_facture_lamy: "123456789",
+            due_date: "2020-01-01",
+            montant: 100,
+            objet_id: 1,
+            libelle_id: 1,
+            tva_id: 1,
+        }
+      });
+        assert.throws(() => {
+            validFormBodyFacture(request.body);
+          }, Error, "Date de facture obligatoire"
+        );
+          
+    });
+}  
+);
+
+// Test sans libelle_id
+describe('facture.controller', () => {
+    it('validFormBodyFacture should return false with empty libelle_id', () => {
+      let request = httpMocks.createRequest({
+        body: {
+            num_facture: "123456789",
+            num_facture_lamy: "123456789",
+            facture_date: "2020-01-01",
+            due_date: "2020-01-01",
+            montant: 100,
+            objet_id: 1,
+            libelle_id: "",
+            tva_id: 1,
+        }
+      });
+        assert.throws(() => {
+            validFormBodyFacture(request.body);
+          }, Error, "Libellé obligatoire"
+        );
+          
+    });
+}
+);
+
+// Test sans tva_id
+describe('facture.controller', () => {
+    it('validFormBodyFacture should return false with empty tva_id', () => {
+      let request = httpMocks.createRequest({
+        body: {
+            num_facture: "123456789",
+            num_facture_lamy: "123456789",
+            facture_date: "2020-01-01",
+            due_date: "2020-01-01",
+            montant: 100,
+            objet_id: 1,
+            libelle_id: 1,
+            tva_id: "",
+        }
+      });
+        assert.throws(() => {
+            validFormBodyFacture(request.body);
+          }, Error, "TVA obligatoire"
+        );
+          
+    });
+}
+);
+
+// Test sans objet_id
+describe('facture.controller', () => {
+    it('validFormBodyFacture should return false with empty objet_id', () => {
+      let request = httpMocks.createRequest({
+        body: {
+            num_facture: "123456789",
+            num_facture_lamy: "123456789",
+            facture_date: "2020-01-01",
+            due_date: "2020-01-01",
+            montant: 100,
+            objet_id: "",
+            libelle_id: 1,
+            tva_id: 1,
+        }
+      });
+        assert.throws(() => {
+            validFormBodyFacture(request.body);
+          }, Error, "Objet obligatoire"
+        );
+          
+    });
+}
+);
+
+// Test sans montant
+describe('facture.controller', () => {
+    it('validFormBodyFacture should return false with empty montant', () => {
+      let request = httpMocks.createRequest({
+        body: {
+            num_facture: "123456789",
+            num_facture_lamy: "123456789",
+            facture_date: "2020-01-01",
+            due_date: "2020-01-01",
+            objet_id: 1,
+            libelle_id: 1,
+            tva_id: 1,
+        }
+      });
+        assert.throws(() => {
+            validFormBodyFacture(request.body);
+          }, Error, "Montant obligatoire"
+        );
+          
+    });
+}
+);
+
+// *********************************************************************************************************************
+// ************************************************** TESTS CREATE LIBELLE *********************************************
+// *********************************************************************************************************************
+// Test avec body complet
+describe('libelle.controller', () => {
+    it('validFormBodyLibelle should return true with full body', () => {
+      let request = httpMocks.createRequest({
+        body: {
+            title: "test",
+     
+        }
+      });
+        let response = httpMocks.createResponse();  
+        validFormBodyLibelle(request.body);
+        assert.equal(response.statusCode, 200);
+
+    });
+}
+);
+
+// Test sans body
+describe('libelle.controller', () => {
+    it('validFormBodyLibelle should return false with empty body', () => {
+      let request = httpMocks.createRequest({
+        body: {
+            
+        }
+      });
+        assert.throws(() => {
+            validFormBodyLibelle(request);
+          }
+        );
+        });
+}
+);
+
+// Test sans title
+describe('libelle.controller', () => {
+    it('validFormBodyLibelle should return false with empty title', () => {
+      let request = httpMocks.createRequest({
+        body: {
+            title: "",
+     
+        }
+      });
+        assert.throws(() => {
+            validFormBodyLibelle(request.body);
+          }, Error, "Titre obligatoire"
+        );
+          
+    });
+}
+);
+
+// *********************************************************************************************************************
+// ************************************************** TESTS CREATE OBJET ***********************************************
+// *********************************************************************************************************************
+
+// Test avec body complet
+describe('objet.controller', () => {
+    it('validFormBodyObjet should return true with full body', () => {
+      let request = httpMocks.createRequest({
+        body: {
+            title: "test",
+     
+        }
+      });
+        let response = httpMocks.createResponse();  
+        validFormBodyObjet(request.body);
+        assert.equal(response.statusCode, 200);
+
+    });
+}
+
+);
+
+// Test sans body
+describe('objet.controller', () => {
+    it('validFormBodyObjet should return false with empty body', () => {
+      let request = httpMocks.createRequest({
+        body: {
+            
+        }
+      });
+        assert.throws(() => {
+            validFormBodyObjet(request);
+          }
+        );
+        });
+}
+
+);
+
+// Test sans title
+describe('objet.controller', () => {
+    it('validFormBodyObjet should return false with empty title', () => {
+      let request = httpMocks.createRequest({
+        body: {
+            title: "",
+     
+        }
+      });
+        assert.throws(() => {
+            validFormBodyObjet(request.body);
+          }, Error, "Titre obligatoire"
+        );
+          
+    });
+}
+
+);
+
+// *********************************************************************************************************************
+// ************************************************** TESTS CREATE TVA *************************************************
+// *********************************************************************************************************************
+
+// Test avec body complet
+describe('tva.controller', () => {
+    it('validFormBodyTva should return true with full body', () => {
+      let request = httpMocks.createRequest({
+        body: {
+            tva_value: "12",
+     
+        }
+      });
+        let response = httpMocks.createResponse();  
+        validFormBodyTva(request.body);
+        assert.equal(response.statusCode, 200);
+
+    });
+}
+
+);
+
+// Test sans body
+describe('tva.controller', () => {
+    it('validFormBodyTva should return false with empty body', () => {
+      let request = httpMocks.createRequest({
+        body: {
+            
+        }
+      });
+        assert.throws(() => {
+            validFormBodyTva(request);
+          }
+        );
+        });
+}
+
+);
+
+// Test sans tva_value
+describe('tva.controller', () => {
+    it('validFormBodyTva should return false with empty tva_value', () => {
+      let request = httpMocks.createRequest({
+        body: {
+            tva_value: "",
+     
+        }
+      });
+        assert.throws(() => {
+            validFormBodyTva(request.body);
+          }, Error, "Value can not be empty!"
+        );
+          
+    });
+}
+
+);
+
+// *********************************************************************************************************************
+// ************************************************** TESTS CREATE USER ************************************************
+// *********************************************************************************************************************
+
+
+
+
+
