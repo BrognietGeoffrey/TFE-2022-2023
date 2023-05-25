c0b63be9ff1ab9bfe34f23e1d0fb84d71d437f46
BrognietGeoffrey <brogniet.geoffrey@gmail.com>
2023-05-25 11:19:40 +0000

    some change

--- server/middleware/jwtAuthentification.js
+++ server/middleware/jwtAuthentification.js
@@ -1,13 +1,9 @@
 const jwt = require('jsonwebtoken');
 const db = require('../models');
 require('dotenv').config();
-
-// get JWT secret key from .env file
 const secretKey = process.env.JWT_SECRET_KEY;
 const {User, Role, UserRoles} = require('../models');
 const { secret } = require('../config/auth.config');
-
-
 
 const tokenVerification = (req, res, next) => {
     let token;
@@ -32,7 +28,6 @@ const tokenVerification = (req, res, next) => {
 const verifyAccess = async() => {
     try{
         const token = req.headers('Authorization').split("Bearer ")[1].trim();
-        console.log(token, "token");
         return jwt.verify(token, secretKey)
     }catch(e){
         return null
@@ -40,7 +35,6 @@ const verifyAccess = async() => {
 }
 
 
-// Function qui vérifie si l'utilisateur est un admin, un moderateur ou un user en paramètre. Et selon le paramètre, on retourne un message d'erreur ou non.
 const verifyIsAdminRole = (req, res, next) => {
     User.findByPk(req.userId).then(user => {
         UserRoles.findOne({
@@ -49,7 +43,6 @@ const verifyIsAdminRole = (req, res, next) => {
             }
         }).then(userRole => {
             Role.findByPk(userRole.roleId).then(role => {
-                console.log(role, "role");
                 if (role.name === "admin") {
                     next();
                     return;
@@ -72,7 +65,6 @@ const verifyIsModeratorRole = (req, res, next) => {
             }
         }).then(userRole => {
             Role.findByPk(userRole.roleId).then(role => {
-                console.log(role, "role");
                 if (role.name === "moderator") {
                     next();
                     return;
@@ -90,14 +82,12 @@ const verifyIsModeratorRole = (req, res, next) => {
 const verifyIsAdminOrModeratorRole = (req, res, next) => {
     // find with decoded id
     User.findByPk(req.userId).then(user => {
-        console.log(user, "user");
         UserRoles.findOne({
             where: {
                 userId: user.userId
             }
         }).then(userRole => {
             Role.findByPk(userRole.roleId).then(role => {
-                console.log(role, "role");
                 if (role.name === "admin" || role.name === "moderator") {
                     next();
                     return;
