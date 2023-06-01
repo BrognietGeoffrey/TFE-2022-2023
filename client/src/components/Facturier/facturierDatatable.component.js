728d1bcf82bf7264edf35a8484ceefee89341a2a
BrognietGeoffrey <geoffrey.brogniet@gmail.com>
2023-06-01 09:05:08 +0200

    change filter factueier

--- client/src/components/Facturier/facturierDatatable.component.js
+++ client/src/components/Facturier/facturierDatatable.component.js
@@ -384,8 +384,9 @@ const FacturierDatatable = () => {
             <div className="flex justify-content-between" id="header">
                         <div className="flex align-items-center export-buttons">
                         <Button type="button" icon="pi pi-file" onClick={() => exportCSV(false)} className="mr-2" tooltip="Exporter toutes les données" tooltipOptions={{ position: 'top' }} />
+                        <Button type="button" icon="pi pi-filter-slash" label="Vider les filtres" className="p-button-outlined" onClick={clearFilter1} />
+
         </div>
-                <Button type="button" icon="pi pi-filter-slash" label="Vider les filtres" className="p-button-outlined" onClick={clearFilter1} />
                 <span className="p-input-icon-left">
                     <i className="pi pi-search" />
                     <InputText value={globalFilterValue1} onChange={onGlobalFilterChange1} placeholder="Rechercher..." />
@@ -418,7 +419,7 @@ const FacturierDatatable = () => {
                      rowHover  loading={loading} dataKey="id" ref={dt} exportFilename={exportFileName}
                     emptyMessage="Aucunes données trouvées." scrollable header={header1} columnResizeMode="expand"  paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
                     currentPageReportTemplate=" {first} de {last} pour {totalRecords} données" responsiveLayout="scroll" style={{ borderRaduis: '20px' }} filters={filters1}
-                    globalFilterFields={['facture.num_facture_lamy,compte_fournisseur.fournisseur.name,compte_fournisseur.fournisseur.num_fournisseur,compte_client.numCompteClient,compte_client.client.name,facture.objet.title,facture.facture_date,facture.libelle.title,decompte.num_decompte,facture.montant,facture.estpaye,facture.due_date,extrait.num_extrait,extrait.date_extrait']}>
+                    globalFilterFields={['facture.num_facture_lamy','compte_fournisseur.fournisseur.name','compte_fournisseur.fournisseur.num_fournisseur','compte_client.numCompteClient','compte_client.client.name','facture.objet.title','facture.facture_date','facture.libelle.title','decompte.num_decompte','facture.montant','facture.estpaye','facture.due_date','extrait.num_extrait','extrait.date_extrait']}>
 
                     <Column field="facture.num_facture_lamy" header="N° de facture Lamy" sortable filter filterPlaceholder="Rechercher par N°" body={numfactureLamyBodyTemplate} style={{ backgroundColor: '#f8f9fa' }}  />
                     <Column field='compte_fournisseur.fournisseur.name' header="Fournisseur" sortable filter filterPlaceholder="Rechercher par nom"  />
